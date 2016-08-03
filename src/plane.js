2fbde7de5a6e9a551abc1daba73b112f047ab912
Sophie Gill <sophie-gill@hotmail.com>
2016-08-03 12:06:33 +0100

    first commit:

---
+++ src/plane.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function Plane() {};
+
+Plane.prototype.land = function(airport) {
+  airport.clearForLanding(this);
+  this._location = airport;
+};
+
+Plane.prototype.takeoff = function(airport) {
+  this._location.clearForTakeOff('');
+};
